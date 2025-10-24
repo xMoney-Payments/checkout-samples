c9f6a876c7c11814f81a150f99d4e64405c6b01d
DanutIlie <42973343+DanutIlie@users.noreply.github.com>
2025-10-24 10:48:57 +0300

    Added card owner verification (#8)
    * Added card owner verification
    
    * upgrade sdk version
    
    * upgrade version
    
    * upgrade sdk
    
    * upgrade sdk
    
    * upgrade sdk
    
    * upgrade sdk

--- src/types/checkout.types.ts
+++ src/types/checkout.types.ts
@@ -13,3 +13,66 @@ export interface InitializeCheckoutResponse {
   checksum: string;
   error?: string;
 }
+
+export interface OrderResponse {
+  id: number;
+  siteId: number;
+  customerId: number;
+  customerData: {
+    id: number;
+    siteId: number;
+    identifier: string;
+    firstName: string;
+    lastName: string;
+    country: string;
+    state: string;
+    city: string;
+    zipCode: string;
+    address: string;
+    phone: string;
+    email: string;
+    isWhitelisted: number;
+    isWhitelistedUntil: string | null;
+    creationDate: string;
+    creationTimestamp: number;
+  };
+  externalOrderId: string;
+  orderType: xMoneyOrderTypeEnum;
+  orderStatus: xMoneyOrderStatusEnum;
+  amount: string;
+  currency: string;
+  description: string;
+  level3Data: null;
+  saveCard: false;
+  invoiceEmail: string;
+  createdAt: string;
+  createdAtTimestamp: number;
+  transactionMethod: xMoneyTransactionMethodEnum;
+  transactionMethodId: number;
+}
+
+export enum xMoneyOrderStatusEnum {
+  Start = "start",
+  InProgress = "in-progress",
+  Retrying = "retrying",
+  Expiring = "expiring",
+  CompleteOk = "complete-ok",
+  CompleteFailed = "completed-failed",
+}
+
+export enum xMoneyTransactionMethodEnum {
+  Card = "card",
+  Wallet = "wallet",
+}
+
+export enum xMoneyOrderTypeEnum {
+  Purchase = "purchase",
+}
+
+export enum MatchStatusEnum {
+  Matched = "MATCHED",
+  NotMatched = "NOT_MATCHED",
+  NotVerified = "NOT_VERIFIED",
+  PartiallyMatched = "PARTIALLY_MATCHED",
+  NotSupported = "NOT_SUPPORTED",
+}
